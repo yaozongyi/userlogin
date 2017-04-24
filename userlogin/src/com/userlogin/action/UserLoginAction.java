0abff212db2cb6634129cc60ad3e925586d81b55
yaozongyi <yaozongyi1986@yahoo.co.jp>
2017-04-24 22:40:44 +0900

    create a new project

---
+++ userlogin/src/com/userlogin/action/UserLoginAction.java
@@ -0,0 +1,16 @@
+package com.userlogin.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserLoginAction extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return "success";
+	}
+}
